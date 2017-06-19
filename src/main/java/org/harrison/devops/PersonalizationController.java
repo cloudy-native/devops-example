9ddc2558b74e833bb64a2cbc378d06a079897d1d
Stephen Harrison <stephen@harrison.org>
2017-06-19 07:33:23 -0400

    Separate controllers and services

--- src/main/java/org/harrison/devops/PersonalizationController.java
+++ src/main/java/org/harrison/devops/PersonalizationController.java
@@ -1,15 +1,9 @@
 package org.harrison.devops;
 
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
-
-import java.math.BigInteger;
 import java.util.List;
 import java.util.Optional;
-import java.util.Random;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,51 +13,36 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @RestController
 @EnableWebMvc
 public class PersonalizationController {
-	/**
-	 * Handy trivial test endpoint
-	 * 
-	 * @param name
-	 * @return
-	 */
-	@RequestMapping("/hello")
+	private final PersonalizationService service;
+
+	@Autowired
+	public PersonalizationController(final PersonalizationService service) {
+		this.service = service;
+	}
+
 	public String hello(@RequestParam(value = "name") Optional<String> name) {
 		return "hello " + name.orElse("there");
 	}
 
 	/**
-	 * Fake a list of similar brand ids
+	 * Return list of similar brand ids
 	 * 
 	 * @param id
 	 * @return
 	 */
 	@RequestMapping("/brand/{brandId}/similar")
 	public List<String> brandSimilar(@PathVariable String brandId) {
-		final Random random = new Random(asSeed("brand" + brandId));
-
-		return Stream.generate(() -> randomString(random)).limit(random.nextInt(5)).collect(toList());
+		return service.brandSimilar(brandId);
 	}
 
 	/**
-	 * Fake a list of recommended brand ids for a member
+	 * Return list of recommended brand ids for a member
 	 * 
 	 * @param id
 	 * @return
 	 */
 	@RequestMapping("/member/{memberId}/brands")
 	public List<String> memberRecommendedBrands(@PathVariable String memberId) {
-		final Random random = new Random(asSeed("member" + memberId));
-		final Supplier<String> randomString = () -> randomString(random);
-
-		return Stream.generate(randomString).limit(random.nextInt(10)).collect(toList());
-	}
-
-	private String randomString(final Random random) {
-		final Supplier<String> fragment = () -> new BigInteger(30, random).toString(Character.MAX_RADIX);
-
-		return Stream.generate(fragment).limit(4).collect(joining("-"));
-	}
-
-	private static long asSeed(final String text) {
-		return new BigInteger(text.getBytes()).longValue();
+		return service.memberRecommendedBrands(memberId);
 	}
 }
